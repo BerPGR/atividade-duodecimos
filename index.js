9b79b0ebccf7b1764a72f434ddf9736ca02d1103
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-10-16 14:33:22 -0300

    Código javascript para calculo das horas

---
+++ index.js
@@ -0,0 +1,37 @@
+/*
+Bernardo de Melo Matuchewski
+Eduardo Mendes Machado
+Gabriel Silva Barros
+Walter Moura e Silva Junior
+
+
+*/
+
+var extensoes = ["", "primeira", "segunda", "terceira", "quarta", "quinta", "sexta", "sétima", "oitava", "nona", "décima", "undécima", "duodécima"]
+const tempoAtual = new Date()
+const horasJudaica = calculaHoras(tempoAtual.getHours())
+const minutosJudaico = calculaMinutos(tempoAtual.getMinutes())
+
+console.log(minutosJudaico + " da " + horasJudaica);
+
+function calculaMinutos(minuto) {
+    return `São ${parseInt(minuto/5)} duodécimos`
+}
+
+function calculaHoras(hora) {
+    let horaNova = 0
+
+    if (hora >=0 && hora<=6) {
+        horaNova = hora + 6
+    } else if (hora >= 7 && hora <= 18) {
+        horaNova = hora - 6
+    } else {
+        horaNova = hora - 18
+    }
+
+    if (hora>=7 && hora <=18) {
+        return `hora ${extensoes[horaNova]} diurna`
+    } else {
+        return `hora ${extensoes[horaNova]} noturna`
+    }
+}
