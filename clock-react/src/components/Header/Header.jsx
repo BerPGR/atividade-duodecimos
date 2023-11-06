71b836c0c9e0b2484b3852def665c84191ff2c28
ugabb <gsb.alternativo@gmail.com>
2023-11-06 11:39:27 -0300

    feat: menu mobile

--- clock-react/src/components/Header/Header.jsx
+++ clock-react/src/components/Header/Header.jsx
@@ -3,14 +3,27 @@ import './Header.css'
 
 // icons
 import { HiOutlineMenuAlt3 } from 'react-icons/hi';
+import MenuMobile from '../MenuMobile/MenuMobile';
 
 
 const Header = () => {
     const [openMenu, setOpenMenu] = useState(false)
+    console.log(openMenu)
+    const handleMenu = () => {
+        setOpenMenu(prev => !prev)
+    }
+
+
+    if (openMenu) {
+        return (
+            <MenuMobile open={openMenu} handleMenu={handleMenu} />
+        )
+    }
+
     return (
         <header className='container-header'>
             <img src='./logo.png' width={100} />
-            <HiOutlineMenuAlt3 className='menu-icon' onClick={setOpenMenu} />
+            <HiOutlineMenuAlt3 className='menu-icon' onClick={handleMenu} />
         </header>
     )
 }
