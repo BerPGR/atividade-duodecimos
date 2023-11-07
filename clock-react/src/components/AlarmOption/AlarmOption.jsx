b2561592cc524bab480fd7335ac38c443634153d
ugabb <gsb.alternativo@gmail.com>
2023-11-07 19:31:08 -0300

    feat: teste de perguntas

--- clock-react/src/components/AlarmOption/AlarmOption.jsx
+++ clock-react/src/components/AlarmOption/AlarmOption.jsx
@@ -1,12 +1,28 @@
 // AlarmOption.js
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./AlarmOption.css";
 import { minutesNumber, hourNumber } from "../../func";
 import useSelect from "../../hook/useSelect";
 import { useForm } from 'react-hook-form'
 import { AlarmContext } from "../context/ContextAlarm";
 
+const questions = [
+  {
+    question: "Digite 3 horas da Tarde",
+    answer: { hora: 15, minutos: 0 }
+  },
+  {
+    question: "Digite 10 horas da noite",
+    answer: { hora: 22, minutos: 0 }
+  },
+  {
+    question: "Digite 2 horas da manha",
+    answer: { hora: 2, minutos: 0 }
+  },
+]
+
 function AlarmOption() {
+  const [questionNumber, setQuestionNumber] = useState(0);
   const {
     setHourAnalog,
     setMinutesAnalog,
@@ -30,10 +46,23 @@ function AlarmOption() {
     console.log(data);
     setHourAnalog(data.horas);
     setMinutesAnalog(data.minutos);
+    console.log("ANTES", questionNumber)
+    handleNextQuestion()
+    console.log("DEpois", questionNumber)
   };
+
+  const handleNextQuestion = () => {
+    if (questionNumber < questions.length - 1) {
+      setQuestionNumber(prev => prev + 1)
+    } else {
+      setQuestionNumber(questions.length - 1)
+    }
+  }
 
   return (
     <div className="option-Container">
+      <p>{questions[questionNumber].question}</p>
+
       <form onSubmit={handleSubmit(onSubmit)} className={`wrapper-option form-container`}>
         <select {...register("horas")}>
           <option disabled value="Horas">
